3161608972eb1c3af32acaae4c120c034cc3d6fb
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-03-23 17:10:00 -0300

    Create mesesdoano.java

---
+++ mesesdoano.java
@@ -0,0 +1,52 @@
+package projetosbasicos;
+
+import java.util.Scanner;
+
+public class CaseBreakMesesDoAno {
+public static void main(String[] args) {
+	Scanner teclado = new Scanner(System.in);
+	System.out.print("Escreva um numero de 1 a 12 respectivamente: ");
+	int mes = teclado.nextInt();
+	
+	switch (mes) {
+	case 1:
+		System.out.println("janeiro");
+		break;
+	case 2:
+		System.out.println("fevereiro");
+		break;
+	case 3:
+		System.out.println("marco");
+		break;
+	case 4:
+		System.out.println("abril");
+		break;
+	case 5:
+		System.out.println("maio");
+		break;
+	case 6:
+		System.out.println("junho");
+		break;
+	case 7:
+		System.out.println("julho");
+		break;
+	case 8:
+		System.out.println("agosto");
+		break;
+	case 9:
+		System.out.println("setembro");
+		break;
+	case 10:
+		System.out.println("outubro");
+		break;
+	case 11:
+		System.out.println("novembro");
+		break;
+	case 12:
+		System.out.println("dezembro");
+		break;
+	
+	}
+	
+}
+}
