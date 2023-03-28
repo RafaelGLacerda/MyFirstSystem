e250efbcd9e2e24b227f793481f44886abb39633
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-03-28 14:50:14 -0300

    Create PrimoOuNao.java

---
+++ PrimoOuNao.java
@@ -0,0 +1,30 @@
+package projetosbasicos;
+
+import java.util.Scanner;
+
+public class PrimoOuNao {
+public static void  main(String[]args) {
+	
+	System.out.println("Esse sistema vai informar se o numero digitado e primo ou nao");
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Digite um numero inteiro de sua escolha: ");
+	int numero = sc.nextInt();
+	
+	 boolean ehPrimo = true;
+     for (int i = 2; i <= numero / 2; i++) {
+         if (numero % i == 0) {
+             ehPrimo = false;
+             break;
+         }
+     }
+
+     if (ehPrimo) {
+         System.out.println(numero + " e um numero primo");
+     } else {
+         System.out.println(numero + " nao e um numero primo");
+     }
+ }
+	
+}
+
