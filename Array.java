3768271ee819f68970d12cf1f1a73250c6cc5ce3
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-03-28 22:09:14 -0300

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,34 @@
+package projetosbasicos;
+
+import java.util.Scanner;
+
+public class array {
+
+	    public static void main(String[] args) {
+	        Scanner input = new Scanner(System.in);
+
+	        System.out.print("Digite o tamanho do array: ");
+	        int tamanho = input.nextInt();
+
+	        int[] numeros = new int[tamanho];
+
+	        for (int i = 0; i < tamanho; i++) {
+	            System.out.print("Digite o " + (i + 1) + " numero: ");
+	            numeros[i] = input.nextInt();
+	        }
+
+	        int somaPar = 0;
+	        int somaImpar = 0;
+
+	        for (int numero : numeros) {
+	            if (numero % 2 == 0) {
+	                somaPar += numero;
+	            } else {
+	                somaImpar += numero;
+	            }
+	        }
+
+	        System.out.println("A soma dos numeros pares e: " + somaPar);
+	        System.out.println("A soma dos numeros impares e: " + somaImpar);
+	    }
+	}
