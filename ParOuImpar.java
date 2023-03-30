d2f9e985c1dd2a3fdf6943eb6c9253728f02920b
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-03-29 22:51:59 -0300

    Update ParOuImpar.java

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -9,7 +9,9 @@ public class ParOuImpar {
 
 	        System.out.print("Digite um numero inteiro: ");
 	        int numero = scanner.nextInt();
-
+                 
+		    
+		// SE O NUMERO FOR DIVISOR DE 2 = 0 ENTÃO ELE É PAR    
 	        if (numero % 2 == 0) {
 	            System.out.println("O numero " + numero + " e par.");
 	        } else {
