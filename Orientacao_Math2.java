46eee20c7557eb76bdfa02e1ed4e55095f156f90
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-04 15:42:30 -0300

    Create Orientacao_Math2.java

---
+++ Orientacao_Math2.java
@@ -0,0 +1,16 @@
+package projetosbasicos;
+
+public class SobrecargaSecundario {
+
+public static double elevar(double i) {
+	return i * i;
+}
+public static double elevar(double i, double j) {
+	return Math. pow(i,j);
+}
+
+
+
+
+
+}
