0d7c6b7c8d72088286cf8800b98979d47db37a3b
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-04 16:07:57 -0300

    Create Parametros2.java

---
+++ Parametros2.java
@@ -0,0 +1,27 @@
+package projetosbasicos;
+
+
+
+public class parametros2 {
+	
+	
+    
+
+public  void mensagem(String nome) {
+	
+	System.out.println();
+	
+	System.out.print("Bem-vindo(a), " +nome);
+	
+}
+
+public void mensagem2(String nome, int idade) {
+	
+	System.out.println();
+	
+	System.out.println("Seu nome e: " +nome+ " e voce possui: " +idade+ " anos");
+	
+	
+}
+
+}
