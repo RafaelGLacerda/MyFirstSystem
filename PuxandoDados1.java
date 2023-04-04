22b925d569554f1ba1917be333954de720b27300
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-04 15:28:38 -0300

    Create PuxandoDados1.java

---
+++ PuxandoDados1.java
@@ -0,0 +1,16 @@
+package projetosbasicos;
+
+public class Delete1 {
+public static void main(String[] args) {
+	
+	Delete2 principal = new Delete2();
+	
+	principal.dadosDelete2();
+	
+	System.out.println("Nome: " +principal.nome);
+	System.out.println("cpf: " +principal.cpf);
+	System.out.println("data de nascimento: " +principal.datadenasc);
+	
+	
+}
+}
