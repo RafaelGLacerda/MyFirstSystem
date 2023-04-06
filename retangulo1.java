802d494e39df6680e860c18bc966d1f51b8b324b
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-06 18:40:00 -0300

    Create retangulo1.java

---
+++ retangulo1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class retangulo2 {
+    public void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Digite o valor da altura do retangulo: ");
+        double altura = sc.nextDouble();
+    
+        System.out.println("Digite o valor da largura do retangulo: ");
+        double largura = sc.nextDouble();
+
+        retangulo easy = new retangulo(largura,altura);
+    
+        System.out.println("Valor do Perimetro: ");
+        easy.exibirperimetro1(largura, altura);
+    
+        System.out.println();
+    
+        System.out.println("Valor da Área: ");
+        easy.exibirarea1(largura, altura);
+    }
+    
+    }
