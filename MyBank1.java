0d8a960f4510a244dd5ffc85d037d733345e1229
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-06 22:14:09 -0300

    Create MyBank1.java

---
+++ MyBank1.java
@@ -0,0 +1,15 @@
+public class ContaBancaria2 {
+    public static void main(String[]args){
+
+    ContaBancaria1 easy = new ContaBancaria1();
+
+    easy.titular();
+    easy.numero();
+    easy.saldo();
+    easy.depositar();
+    easy.sacar();
+    easy.saldoAtualizado();
+    easy.exibir();
+    
+    }
+}
