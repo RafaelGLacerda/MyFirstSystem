64e1f99a8d17e8b6fa5b0f7f3f6cb58cdc63874b
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-06 22:14:30 -0300

    Create MyBank2.java

---
+++ MyBank2.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ContaBancaria1{
+
+private static int numero;
+private static double saldo;
+private static String titular;
+private static double depositar;
+private static double sacar;
+
+Scanner sc = new Scanner(System.in);
+
+public void titular(){
+System.out.print("Digite o nome do titular: ");
+titular = sc.nextLine();
+}
+public void numero(){
+System.out.print("Digite o numero do titular: ");
+ numero = sc.nextInt();
+}
+public void saldo(){
+System.out.print("Digite o valor do seu saldo: ");
+saldo = sc.nextDouble();
+}
+public void depositar(){
+System.out.print("Digite o valor que deseja depositar: ");
+depositar = sc.nextDouble();
+
+}
+public void sacar(){
+System.out.print("Digite o valor que deseja sacar: ");
+sacar = sc.nextDouble();
+
+}
+public void saldoAtualizado(){
+saldo += depositar;
+saldo -= sacar;
+}
+public void exibir(){
+    System.out.println("Dados Após Atualização: ");
+    System.out.println("Titular: " +titular);
+    System.out.println("Numero: " +numero);
+    System.out.println("Saldo: " +saldo);
+}
+
+
+
+}
