2f03e1bb38c89249b901d8e9f933a467226d4324
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-06 22:47:14 -0300

    Create Cadastro1.java

---
+++ Cadastro1.java
@@ -0,0 +1,17 @@
+public class people2 {
+    public static void main(String[] args){
+
+    people1 easy = new people1();
+
+    easy.nome();
+    easy.idade();
+    easy.cpf();
+    easy.sexo();
+
+    System.out.println();
+    
+    easy.exibir();
+    easy.MostrarIdade();
+
+    }
+}
