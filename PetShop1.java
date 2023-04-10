e64de80ce0dce7333912d24c214a3991bd260a8c
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-10 16:20:27 -0300

    Create PetShop1.java

---
+++ PetShop1.java
@@ -0,0 +1,20 @@
+public class CadastroPet2 {
+    public static void main(String[] args){
+
+    CadastroPet1 easy = new CadastroPet1();
+
+    easy.animal();
+    easy.nome();
+    easy.especie();
+    easy.sexo();
+    easy.cores();
+    easy.idade();
+    easy.peso();
+    easy.altura();
+    easy.comprimento();
+    easy.descrição();
+    
+    easy.exibir();
+
+    }
+}
