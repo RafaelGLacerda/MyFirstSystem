614355679fad6a29a6a04c0b9651ab3736885b64
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-10 15:50:03 -0300

    Create TrianguloOrientado1.java

---
+++ TrianguloOrientado1.java
@@ -0,0 +1,15 @@
+public class triangulo2 {
+    public static void main(String[] args){
+
+    triangulo1 easy = new triangulo1();
+
+    easy.base();
+    easy.altura();
+    easy.lado1();
+    easy.lado2();
+    easy.lado3();
+    easy.perimetro();
+    easy.area();
+
+    }
+}
