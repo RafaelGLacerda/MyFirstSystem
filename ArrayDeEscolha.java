4f87eee850da1ca5dc94cda71766a3566000c4bc
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-11 15:16:40 -0300

    Create ArrayDeEscolha.java

---
+++ ArrayDeEscolha.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class arrayXD {
+   
+    public static void main(String[] args){
+    
+    int[] numeros = new int[100000];
+
+    Arrays.fill(numeros, 12);
+    
+    System.out.println(Arrays.toString(numeros));
+
+    }
+}
