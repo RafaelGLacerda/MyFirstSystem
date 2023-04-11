e04e4ca238aa5f585745e26d32ec8b59e922850e
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-10 22:19:50 -0300

    Create UsandoVar.java

---
+++ UsandoVar.java
@@ -0,0 +1,14 @@
+public class usandovar{
+public static void main(String[] args){
+
+var x = "Rafael";
+var y = 5;
+var z = 10;
+  
+// var - estender a inferência de tipos para declarações de variáveis locais com inicializadores. Isso reduz o código repetitivo.
+  
+System.out.println(y+z+x);
+
+
+}
+}
