04ec2f4ba2b138b155c8a7aa13682872be466bc8
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-12 20:49:42 -0300

    Create ConversorDistanciaTemp1.java

---
+++ ConversorDistanciaTemp1.java
@@ -0,0 +1,14 @@
+public class conversortotal2 {
+    public static void main(String[] args){
+
+        conversortotal1 easy = new conversortotal1();
+
+
+    easy.celciusParaFahrenheit();
+    easy.fahrenheitParaCelcius();
+    easy.quilometrosParaMilhas();
+    easy.milhasParaQuilometros();
+
+
+    }
+}
