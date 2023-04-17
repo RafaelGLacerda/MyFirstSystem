9c08bf7bbdd1b99ab7eab0b852b036811ee3ea42
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-17 14:54:50 -0300

    Update MapArray.java

--- MapArray.java
+++ MapArray.java
@@ -1,27 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ArrayList0{
+public class ArrayMap {
     public static void main(String[] args){
 
-List<String> listaDeTarefas = new ArrayList<>();
-listaDeTarefas.add("Fazer compras no mercado");
-listaDeTarefas.add("Lavar louça");
-listaDeTarefas.add("Estudar programação");
-listaDeTarefas.add("ir pra faculdade");
-listaDeTarefas.add("etc, etc");
+Map<String, Double> notaDosAlunos = new HashMap<>();
+notaDosAlunos.put("Alice", 8.5);
+notaDosAlunos.put("Bob", 5.6);
+notaDosAlunos.put("Fernando", 7.5);
+notaDosAlunos.put("Albert", 6.4);
+notaDosAlunos.put("Fabrizzio", 9.2);
 
-listaDeTarefas.remove(1); // remove um item da lista.
-
-System.out.println(listaDeTarefas.get(0));
-System.out.println(listaDeTarefas.get(1));
-System.out.println(listaDeTarefas.get(2));
-System.out.println(listaDeTarefas.get(3));
-System.out.println(listaDeTarefas.get(4));
-System.out.println(listaDeTarefas.get(5));
-
-
-
+System.out.println(notaDosAlunos.get("Alice"));
+System.out.println(notaDosAlunos.get("Bob"));
+System.out.println(notaDosAlunos.get("Fernando"));
+System.out.println(notaDosAlunos.get("Albert"));
+System.out.println(notaDosAlunos.get("Fabrizzio"));
 
     }
 }
