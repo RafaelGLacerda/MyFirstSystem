dc3545d674d0b1fdeeda8cf711423f0556317c4b
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-17 15:46:33 -0300

    Create ThreadsExemplo.java

---
+++ ThreadsExemplo.java
@@ -0,0 +1,25 @@
+public class ThreadsExemplo extends Thread {
+    private String mensagem;
+
+    public ThreadsExemplo(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(mensagem);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrompida");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadsExemplo thread1 = new ThreadsExemplo("Thread 1");
+        ThreadsExemplo thread2 = new ThreadsExemplo("Thread 2");
+        thread1.start();
+        thread2.start();
+    }
+}
