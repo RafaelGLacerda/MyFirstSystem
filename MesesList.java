ed8d241bc3cbe1620848970494b614a8a69c65da
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-18 14:21:04 -0300

    Create MesesList.java

---
+++ MesesList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class xinzhao{
+    public static void main(String[] args){
+
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Janeiro");
+        nomes.add("fevereiro");
+        nomes.add("março");
+        nomes.add("abril");
+        nomes.add("maio");
+        nomes.add("junho");
+        nomes.add("julho");
+        nomes.add("agosto");
+        nomes.add("setembro");
+        nomes.add("outubro");
+        nomes.add("novembro");
+        nomes.add("dezembro");
+
+
+    System.out.println(nomes.get(0));
+    System.out.println(nomes.get(1));
+    System.out.println(nomes.get(2));
+    System.out.println(nomes.get(3));
+    System.out.println(nomes.get(4));
+    System.out.println(nomes.get(5));
+    System.out.println(nomes.get(6));
+    System.out.println(nomes.get(7));
+    System.out.println(nomes.get(8));
+    System.out.println(nomes.get(9));
+    System.out.println(nomes.get(10));
+    System.out.println(nomes.get(11));
+
+}
+}
