5fdf861d867bc2a07c91ba29717f23b01d77cf6c
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-21 13:01:57 -0300

    Create ArrayMinMax.java

---
+++ ArrayMinMax.java
@@ -0,0 +1,38 @@
+public class ArrayMinMax{
+    public static void main(String[] args){
+
+int[] array = {3, 5, 20, 250, 33};
+
+if((array[0] > array[1]) && (array[0] > array[2]) && (array[0] > array[3]) && (array[0] > array[4])){
+System.out.println("Maior numero = " +array[0]);
+}
+if((array[1] > array[0]) && (array[1] > array[2]) && (array[1] > array[3]) && (array[1] > array[4])){
+System.out.println("Maior numero = " +array[1]);
+}
+if((array[2] > array[0]) && (array[2] > array[1]) && (array[2] > array[3]) && (array[2] > array[4])){
+System.out.println("Maior numero = " +array[2]);
+}
+if((array[3] > array[0]) && (array[3] > array[1]) && (array[3] > array[2]) && (array[3] > array[4])){
+System.out.println("Maior numero = " +array[3]);
+}
+if((array[4] > array[0]) && (array[4] > array[1]) && (array[4] > array[2]) && (array[4] > array[3])){
+System.out.println("Maior numero = " +array[4]);
+}
+if((array[0] < array[1]) && (array[0] < array[2]) && (array[0] < array[3]) && (array[0] < array[4])){
+System.out.println("Menor numero = " +array[0]);
+}
+if((array[1] < array[0]) && (array[1] < array[2]) && (array[1] < array[3]) && (array[1] < array[4])){
+System.out.println("Menor numero = " +array[1]);
+}
+if((array[2] < array[0]) && (array[2] < array[1]) && (array[2] < array[3]) && (array[2] < array[4])){
+System.out.println("Menor numero = " +array[2]);
+}
+if((array[3] < array[0]) && (array[3] < array[1]) && (array[3] < array[2]) && (array[3] < array[4])){
+System.out.println("Menor numero = " +array[3]);
+}
+if((array[4] < array[0]) && (array[4] < array[1]) && (array[4] < array[2]) && (array[4] < array[3])){
+System.out.println("Menor numero = " +array[4]);
+}
+
+    }
+}
