d585dae5a492277d236ce3c0f8ea3ebefb224bd4
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-04-28 15:32:49 -0300

    Create CalculadoraOrientada.java

---
+++ CalculadoraOrientada.java
@@ -0,0 +1,80 @@
+abstract class operações {
+    public abstract void soma();
+    public abstract void sub();
+    public abstract void multi();
+    public abstract void div();
+
+}
+
+class somar extends operações {
+    private double n1;
+    private double n2;
+
+    public somar(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public double soma() {
+        return n1 + n2;
+    }
+}
+
+class subtrair extends operações {
+    private double n1;
+    private double n2;
+
+    public subtrair(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public double sub() {
+        return n1 - n2;
+    }
+}
+
+class multiplicar extends operações {
+    private double n1;
+    private double n2;
+
+    public multiplicar(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public double multi() {
+        return n1 * n2;
+    }
+}
+
+class divisão extends operações {
+    private double n1;
+    private double n2;
+
+    public divisão(double n1, double n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public double div() {
+        return n1 / n2;
+    }
+}
+
+public class Calculadora {
+    public static void main(String[] args) {
+
+somar mais = new somar(5, 5);
+subtrair menos = new subtrair(10, 8);
+multiplicar vezes = new multiplicar(7, 8);
+divisão dividir = new divisão(50, 10);
+
+System.out.println("Soma = " mais.somar(););
+System.out.println("Subtração = " menos.sub(););
+System.out.println("Multiplicação = " vezes.multi(););
+System.out.println("Divisão = " dividir.div(););
+
+
+    }
+}
