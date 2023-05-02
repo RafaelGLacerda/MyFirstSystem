a3a03bd23d7a636c85ebce8cbc2f8ae3d4b73e1f
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-05-02 17:58:45 -0300

    Update FiltroDePesquisa.java

--- FiltroDePesquisa.java
+++ FiltroDePesquisa.java
@@ -1,11 +1,30 @@
-public class Arrayson6 {
+public class FiltroArray {
     public static void main(String[] args){
 
-String[] Palavras = {"pessego ", "lapis", "livro", "pessoa", "irmão", "diario", "Passos", "Fazer", "Palavra"};
 
+String[] Palavras = {"bateria", "borracha", "função", "breve", "porta", "pessoa", "Bruno", "fraco", "Pedro", "Paulo", "frio", "promoção"};
+
+
+System.out.println("-----> Palavras começando com 'P': <-----");
 for(int i = 0; i < Palavras.length; i++){
     char letra = Palavras[i].charAt(0);
     if(letra == 'p' || letra == 'P'){
+        System.out.println(Palavras[i]);
+    }
+}
+System.out.println();
+System.out.println("-----> Palavras começando com 'F': <-----");
+for(int i = 0; i < Palavras.length; i++){
+    char letra = Palavras[i].charAt(0);
+    if(letra == 'f' || letra == 'F'){
+        System.out.println(Palavras[i]);
+    }
+}
+System.out.println();
+System.out.println("-----> Palavras começando com 'B': <-----");
+for(int i = 0; i < Palavras.length; i++){
+    char letra = Palavras[i].charAt(0);
+    if(letra == 'b' || letra == 'B'){
         System.out.println(Palavras[i]);
     }
 }
