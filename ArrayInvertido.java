6416c67211779e28088f076263629d85de794d01
Rafael Lacerda <128004902+RafaelGLacerda@users.noreply.github.com>
2023-05-03 18:57:35 -0300

    Create ArrayInvertido.java

---
+++ ArrayInvertido.java
@@ -0,0 +1,11 @@
+public class Arrayson13 {
+    public static void main(String[] args){
+
+        int[] numerunn = {3, 42, 63, 86, 32, 232, 532, 643, 100, 43, 545, 49, 0, 34};
+
+    for(int i = numerunn.length -1; i >= 0; i--){
+        System.out.print(numerunn[i] + " ");
+    }
+     
+    }
+}
